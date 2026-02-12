36d74688a3f37ca2c8c62bbab277f28eeb1a6745
Kim Hunt <khunt2715@gmail.com>
2026-02-12 14:43:10 -0500

    Populating Reference Data with a sample list of High Risk Countries and a synthetic Sanctions List

--- sql/03_reference_data.sql
+++ sql/03_reference_data.sql
@@ -1,1 +1,21 @@
--- Reference Table
+-- 03_reference_data.sql 
+-- Adding values for High Risk Countries and a Sactions List
+
+INSERT INTO high_risk_countries VALUES
+('Iran'),('North Korea'),('Syria'),
+('Myanmar'),('Afghanistan'),
+('Russia'),('Venezuela'),
+('Somalia'),('Yemen'),('Libya');
+
+INSERT INTO sanctions_list VALUES
+('Global Export LLC'),
+('Eastern Trading Co'),
+('Al Noor Holdings'),
+('Black Sea Industries'),
+('Desert Falcon Ltd'),
+('Caspian Ventures'),
+('Northern Star Corp'),
+('Atlas Shipping Group'),
+('Red Horizon Ltd'),
+('Silver Crescent SA');
+
