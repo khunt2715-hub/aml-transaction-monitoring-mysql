2cd97b2e7bd80e45eaffb6f6b67b26ea6c33e7ef
Kim Hunt <khunt2715@gmail.com>
2026-02-12 16:12:43 -0500

    Injected suspicious transactions into synthetic transaction table for rule testing

--- sql/07_injected_patterns.sql
+++ sql/07_injected_patterns.sql
@@ -1,1 +1,23 @@
--- Injected Patterns
+-- Injecting Suspicious Behaviour into the Transaction table 
+-- The purpose is to ensure that the Structuring rule will trigger as intended
+
+INSERT INTO transactions (
+    account_id,
+    transaction_date,
+    transaction_type,
+    amount,
+    counterparty_country,
+    counterparty_name,
+    is_cash
+)
+SELECT
+    account_id,
+    NOW(),
+    'credit',
+    9500,
+    'USA',
+    'Structuring_Source',
+    TRUE
+FROM accounts
+WHERE customer_id IN (10,20,30,40,50)
+LIMIT 20;
