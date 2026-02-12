49a8af704418d5ec9511bac55447fe665b640d37
Kim Hunt <khunt2715@gmail.com>
2026-02-12 15:06:27 -0500

    Generated a synthetic list of 1500 accounts

--- sql/05_accounts.sql
+++ sql/05_accounts.sql
@@ -1,1 +1,11 @@
---Accounts Table
+-- Generating a list of 1500 accounts using synthetic data
+
+INSERT INTO accounts (customer_id, account_type, balance, open_date)
+SELECT
+    FLOOR(1 + RAND() * 1000), -- random customer_id between 1 and 1000
+    ELT(FLOOR(1 + RAND() * 3), 'Checking','Savings','Investment'),
+    ROUND(RAND() * 50000, 2),
+    DATE_SUB(CURDATE(), INTERVAL FLOOR(RAND()*1000) DAY)
+FROM numbers
+WHERE n <= 1500;
+
